014a15e0c6e4b48f8b96fbc02567583df4c90bde
yuivae <gnyserhan@gmail.com>
2021-08-04 14:05:28 +0200

    api-key changed

--- pages/contact.js
+++ pages/contact.js
@@ -32,7 +32,7 @@ export default function contact() {
 
     try {
       setIsLoading(true);
-      const response = await fetch("https://www.bensem.eu/api/email", {
+      const response = await fetch("/api/email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
